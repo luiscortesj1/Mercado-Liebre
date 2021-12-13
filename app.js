18941c2dc39919a4259521a0f4946186b3f0a5a9
luiscortesj1 <luiscortesj1999@gmail.com>
2021-12-13 17:51:45 -0500

    register-css

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path'); // funcion
 const publicPath = path.join(__dirname, 'public')// resolver ruta
 app.use(express.static(publicPath)); //traer los elementos public - recursos estaticos
 
-app.listen(3001,()=>{  //correr servidor
+app.listen(3000,()=>{  //correr servidor
     console.log("Server Run")
 })
 
